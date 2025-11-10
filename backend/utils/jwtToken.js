32c3180b7bc8b1730e973fa40119534c26b82c43
Ritupagar12 <ritupagar3@gmail.com>
2025-11-11 10:43:00 +1300

    Updated jwtToken to send cookies

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -6,13 +6,20 @@ export const sendToken = (user, statusCode, message, res) => {
     });
 
     // Set cookie and send response
-    res.status(statusCode).cookie("token", token, {
-        expires: new Date(Date.now() + process.env.COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
-        httpOnly: true, // Cookie cannot be accessed by JS in browser
-    }).json({
-        success: true,
-        user, // Return user object
-        message, // Custom success message
-        token, // Return token as well (optional)
-    });
-}
+    res
+        .status(statusCode).
+        cookie("token", token, {
+            expires: new Date(
+                Date.now() + process.env.COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+            ),
+            httpOnly: true, // Cookie cannot be accessed by JS in browser
+            secure: process.env.NODE_ENV === "production", // required for https
+            sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", //Allows cross-site cookies
+        })
+        .json({
+            success: true,
+            user, // Return user object
+            message, // Custom success message
+            token, // Return token as well (optional)
+        });
+};
